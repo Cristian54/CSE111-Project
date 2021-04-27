2d2aec05ac766b9a0b37967bd9dc1cbf3e5082c9
cgalvan9 <cgalvan9@ucmerced.edu>
2021-04-27 15:17:01 -0700

    Monthly report based on user input

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, g
+from flask import Flask, render_template, url_for, request
 import sqlite3
 from flask_sqlalchemy import SQLAlchemy
 
@@ -14,7 +14,7 @@ class Rain(db.Model):
     __tablename__ = 'SeattleRainfall'
     __table_args__ = { 'extend_existing': True }
     DATE = db.Column(db.Date, primary_key=True) 
-    PRCP = db.Column(db.Numeric)
+    PRCP = db.Column(db.Float)
     TMAX = db.Column(db.Integer) 
     TMIN = db.Column(db.Integer)  
     RAIN = db.Column(db.Boolean)
@@ -23,46 +23,24 @@ class Rain(db.Model):
 @app.route('/')
 def home_page():
     days = Rain.query.count()
-    hotDays = Rain.query.filter_by(TMAX = 98).all()
+    #hotDays = Rain.query.filter_by(TMAX = 98).all()
+    #oneMonth = Rain.query.filter(Rain.DATE.startswith('1948-01')).all()
     
-    for hd in hotDays:
-        print("Date: ", hd.DATE, " Temp: ", hd.TMAX)
+    #for day in oneMonth:
+        #print("Date: ", day.DATE, "Max Temp: ", day.TMAX)
          
-    return render_template('home.html', days = days, hotDays = hotDays)
+    return render_template('home.html', days = days)
 
 
-'''
-@app.route('/listReport', methods = ['GET', 'POST'])
-def listReport():
+@app.route('/monthlyReport', methods = ['POST', 'GET'])
+def monthlyReport():
     if request.method == 'GET':
-        year = request.form["year"]
-        month = request.form["month"]
-        date = year + "-" + month
-    
-        sql = """
-        SELECT *
-        FROM SeattleRainfall
-        WHERE strftime('%Y-%m', DATE) = ?
-        GROUP BY strftime('%d', DATE) """
-    
-        cur = get_db().cursor()
-        cur.execute(sql, (date,))
-        report = cur.fetchall()
-        #rows = getDailyReport(conn, year, month)
-        cur.close()
-        
-        for row in report:
-            if row[4] == 'TRUE':
-                rain = 'Rained'
-            else:
-                rain = 'Did not rain'
-            print("Date:", row[0], "|", rain, "| Precipitation:", row[1], "| Highest/Lowest temperature (F):", row[2], "/", row[3])
-        
-        
-        return render_template('list.html', rows = report)
-    else:
-        print("this isn't a post request")
-    '''
+        return f"The URL /data is accessed directly. Try going to '/' to submit form"
+    if request.method == 'POST':
+        form = request.form
+        yearMonth = form.get('Year') + "-" + form.get('Month')
+        report = Rain.query.filter(Rain.DATE.startswith(yearMonth)).all()
+        return render_template('monthlyReport.html', monthlyReport = report, yearMonth = yearMonth)
 
 if __name__ == "__main__":
     app.run(debug=True)
